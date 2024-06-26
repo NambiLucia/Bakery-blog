b470e4514c5030e520b432b5fcb97dba1aac9962
lucia nambi <nambilucia@gmail.com>
2024-06-26 13:36:03 +0300

    complete recipe section

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -3,6 +3,7 @@ import Header from './Header';
 import Footer from './Footer';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { FaArrowAltCircleDown } from "react-icons/fa";
 
  function Recipe() {
   const [recipe,setRecipe] =useState([]);
@@ -26,12 +27,18 @@ import { Link } from 'react-router-dom';
          {recipe.length >0? (
           recipe.map((element) =>{
             return(
-              <div key={element.id} className='flex flex-wrap '>
+              <div key={element.id} className='flex flex-wrap items-center justify-center relative'>
                 <img 
                         src={`http://localhost:1337${element.attributes.image.data[0].attributes.url}`}
                         alt="image"
-                        className='flex w-1/2 h-80 object-cover m-8 rounded-md transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 duration-300'
+                        className='flex flex-wrap w-full h-60 object-cover m-8 rounded-md transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 duration-300 justify-center items-center
+                        opacity-80'
                 />
+                <div className='absolute'>
+                  <h2 className='font-Cinzel text-lg font-bold'>Check out {element.attributes.type} <br />
+                 <Link to={element.attributes.link}> <FaArrowAltCircleDown
+                  className='mx-auto animate-bounce m-12 font-bold text-4xl'/></Link></h2>
+                </div>
                 <div className='flex flex-wrap justify-center items-center mx-auto'>    
                   <Link to={element.attributes.link}><h2 className='hover:underline text-center text-lg font-bold m-8 font-Cinzel '>{element.attributes.type}</h2>
                           </Link> 
